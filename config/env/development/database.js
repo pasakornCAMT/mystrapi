cdd270253f4c1d573bbfbd3a8ced7cbfe721f5fa
pasakornCRMC <pasakorn@crmc.consulting>
2022-10-15 18:39:39 +0700

    updated yml

--- config/env/development/database.js
+++ config/env/development/database.js
@@ -1,9 +1,9 @@
 module.exports =  ({ env }) => ({
 	connection: {
-		client: 'mysql',
+		client: 'postgres',
 		connection: {
 		host: env('DATABASE_HOST', 'localhost'),
-			port: env.int('DATABASE_PORT', 3306),
+			port: env.int('DATABASE_PORT', 5432),
 			database: env('DATABASE_NAME', 'strapi'),
 			user: env('DATABASE_USERNAME', 'strapi'),
 			password: env('DATABASE_PASSWORD', '1q2w3e4r5t'),
